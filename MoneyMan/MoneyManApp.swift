224177e0606fad6473fa858e3e9b31e22ea91324
Mike JR <mikedalvik@ya.ru>
2022-05-29 13:26:58 +0200

    Initial Commit

---
+++ MoneyMan/MoneyManApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManApp.swift
+//  MoneyMan
+//
+//  Created by Mike Shevelinsky on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
